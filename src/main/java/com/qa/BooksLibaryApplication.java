4dcbc677997974458801cb4cfbb7308e0794d0f8
mubarik <mubarikabdi1@icloud.com>
2022-09-10 23:44:52 +0100

    first

---
+++ src/main/java/com/qa/BooksLibaryApplication.java
@@ -0,0 +1,13 @@
+package com.qa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksLibaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksLibaryApplication.class, args);
+	}
+
+}
